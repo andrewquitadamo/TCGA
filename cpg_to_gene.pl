8862c5ce00e7f2b4b0d879ca88630199c41c18b4
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2014-07-18 19:22:33 -0400

    cpg_to_gene.pl

---
+++ cpg_to_gene.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my %cpg_gene;
+
+my $filename = "cpg_gene";
+open(FILE,$filename) || die "can't open that $filename";
+while (<FILE>)
+{
+	chomp;
+	my @temp = split("\t",$_);
+	my $cpg = shift(@temp);
+	my $gene = shift(@temp);
+
+	$cpg_gene{$cpg}=$gene;
+}
+close FILE;
+
+$filename=shift(@ARGV);
+open(FILE,$filename) || die "can't open that $filename";
+while (<FILE>)
+{
+	chomp;
+	if ($cpg_gene{$_})
+	{
+		print "$cpg_gene{$_}\n";
+	}
+}
+close FILE;
